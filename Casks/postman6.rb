05dc09604e838025499525020c4f54f306068fe0
jdollar <dollarhidejoel@gmail.com>
2019-09-13 09:43:45 -0400

    Updating sha for newest version of postman 6

--- Casks/postman6.rb
+++ Casks/postman6.rb
@@ -1,6 +1,6 @@
 cask 'postman6' do
   version '6'
-  sha256 'f0ac7764e70eb8e9fd27efb82f69ba8e41b9cb7a3767e1db887b439dff09a029'
+  sha256 '8ede79c2697eca3fd3b8fbed13881fd9d6633559a78da044a7ddd4833fd9831e'
 
   url "https://go.pstmn.io/dl-macos64-v#{version}-latest"
   appcast "https://dl.pstmn.io/update/status?channel=stable&currentVersion=#{version}.0.0&arch=64&platform=osx&syncEnabled=true&teamPlan="
